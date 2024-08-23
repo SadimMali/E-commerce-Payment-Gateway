dfc092d227fff1e4948d007a19dcbaaedc648a29
Sadim Mali <sadimmalakar77@gmail.com>
2024-08-23 17:58:35 +0545

    feat: Modify home page with carousel and search by category

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
+import CuratedForYouItem from "@/components/CuratedForYouItem";
 
 export default function Home() {
   return (
-    <div>
-      home page
-    </div>
+    <main className="px-5 py-5 md:px-24 lg:px-32">
+
+      {/* search by category */}
+      <section className="my-5">
+        <div className="flex justify-between">
+          <p className="font-bold text-lg">Shop by category</p>
+          <span className="text-slate-400 text-md">See All</span>
+        </div>
+        <div className="flex gap-5 pt-2">
+          <div className="flex flex-col items-center">
+            <div className="w-14 h-14 rounded-full bg-slate-700"></div>
+            <p className="font-semibold text-md">text</p>
+          </div>
+        </div>
+      </section>
+      {/* carsouel */}
+       <CuratedForYouItem />
+    </main>
   );
 }
