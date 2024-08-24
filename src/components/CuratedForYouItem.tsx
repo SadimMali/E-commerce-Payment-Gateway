1d4e1b57b6a01a4a0958e328db78c48122232811
Sadim Mali <sadimmalakar77@gmail.com>
2024-08-24 18:00:19 +0545

    refactor: Modify line of statement with ProductCard component

--- src/components/CuratedForYouItem.tsx
+++ src/components/CuratedForYouItem.tsx
@@ -4,15 +4,14 @@ import {
   CarouselContent,
   CarouselItem,
 } from "@/components/ui/carousel";
-import { getRandomProduct, Products, PRODUCTS } from "@/utils/products";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { getRandomProduct, Product, PRODUCTS } from "@/utils/products";
 import Autoplay from "embla-carousel-autoplay";
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import { Star } from "lucide-react";
+import ProductCard from "./ProductCard";
 
 const CuratedForYouItem = () => {
-  const [curatedForYouItem, setCuratedForYoutItem] = useState<Products[]>([]);
+  const [curatedForYouItem, setCuratedForYoutItem] = useState<Product[]>([]);
   useEffect(() => {
     setCuratedForYoutItem(getRandomProduct(PRODUCTS, 6));
   }, []);
@@ -31,21 +30,7 @@ const CuratedForYouItem = () => {
           <CarouselItem key={item.id} className="md:basis-1/2 lg:basis-1/3">
             <div className="p-1">
               <Link href={`/product/${item.id}`}>
-                <Card className="aspect-square h-full">
-                  <CardHeader className="h-full w-full">
-                    <img
-                      src={item.img}
-                      className="max-w-full h-full object-contain"
-                    />
-                  </CardHeader>
-                  <CardContent className="flex flex-col justify-center ">
-                    <div className="flex justify-between items-center">
-                      <p className=" text-sm  text-gray-500"> <span>{item.subCategory}</span> <Star className="inline-block w-5 h-5 mb-1 text-[#edcf5d] fill-[#edcf5d]" /> 4.9 </p>
-                    </div>
-                    <p className="text-lg font-semibold">{item.name}</p>
-                    <p className="font-semibold text-md"> ${item.price}</p>
-                  </CardContent>
-                </Card>
+                <ProductCard item={item} />
               </Link>
             </div>
           </CarouselItem>
