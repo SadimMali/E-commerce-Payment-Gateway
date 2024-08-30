5d619a3b685927f2b3192707690c674b65a02f4d
Sadim Mali <sadimmalakar77@gmail.com>
2024-08-30 19:39:02 +0545

    feat: Modify UserModel schema

--- src/model/User.model.ts
+++ src/model/User.model.ts
@@ -1,26 +1,53 @@
-import mongoose, {Schema, Document} from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
 interface User extends Document {
-    username: string;
-    first_name: string;
-    last_name: string;
-    phone_number: number;
-    location: string;
-    enail: string;
-    pasword: string;
+  username: string;
+  first_name: string;
+  last_name: string;
+  phone_number: number;
+  location: string;
+  email: string;
+  password: string;
 }
 
-
 const UserSchema: Schema<User> = new Schema({
-    username: {
-        type: String,
-        required: [true, "Username is required"],
-        trim: true,
-        unique: true,
-    } //add more....
-})
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    trim: true,
+    unique: true,
+  },
+  first_name: {
+    type: String,
+    required: [true, "First name is required"],
+  },
+  last_name: {
+    type: String,
+    required: [true, "Last name is required"],
+  },
+  phone_number: {
+    type: Number,
+    required: [true, "Phone number is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/.+\@.+\..+/, "Please use a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
 
+  location: {
+    type: String,
+    required: [true, "Location is required"],
+  },
+});
 
-const UserModel = mongoose.models.User as mongoose.Model<User> || mongoose.model("User", UserSchema) 
+const UserModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model("User", UserSchema);
 
 export default UserModel;
