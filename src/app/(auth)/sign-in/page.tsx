ecba159e2245c67e34e8996495b0bacb9a5da417
Sadim Mali <sadimmalakar77@gmail.com>
2024-11-02 22:15:12 +0545

    feat(ui): Implement next-auth's signIn module to sign in page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,124 @@
-const LoginPage = () => {
-  return (
-    <div className=''>LoginPage</div>
-  )
-}
+"use client";
+import { useToast } from "@/components/hooks/use-toast";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { SignIn, signInSchema } from "@/schemas/signInSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import { signIn } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-export default LoginPage
+const LoginPage = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const router = useRouter();
+
+  const { toast } = useToast();
+
+  const form = useForm<SignIn>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: SignIn) => {
+    setIsSubmitting(true);
+    const result = await signIn("credentials", {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password,
+    });
+    console.log(result);
+    if (result?.error) {
+      setIsSubmitting(false);
+      toast({
+        title: "Login failed",
+        description: "Incorrect username or password",
+        variant: "destructive",
+      });
+    }
+
+    if (result?.url) {
+      setIsSubmitting(false);
+      toast({
+        title: "Login successfully",
+        description: "Redirecting to dashboard",
+      });
+      router.replace("/dashboard");
+    }
+  };
+
+  return (
+    <div >
+          <h1 className="text-2xl font-semibold py-4">
+            Enter your email to join us or sign in 
+          </h1>
+        {/* sign in form */}
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="identifier"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email / Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="email/username" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="password" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? (
+                <>
+                  <Loader2 className="w-4 h-4 mr-2 animate-spin" /> Please
+                  wait...
+                </>
+              ) : (
+                "Sign in"
+              )}{" "}
+            </Button>
+          </form>
+        </Form>
+        <p className="text-md text-center mt-4 ">
+          Not a member yet?
+          <Link
+            href="/sign-up"
+            className=" ml-2 font-bold text-blue-600 hover:text-blue-800"
+          >
+            Sign up
+          </Link>
+        </p>
+      </div>
+  );
+};
+
+export default LoginPage;
