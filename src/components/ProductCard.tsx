d45779afe1a353b72ad69fc66963848df50c866b
Sadim Mali <sadimmalakar77@gmail.com>
2024-11-12 21:48:17 +0545

    refactor(ProductCard): Modify the types and its key properties

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,30 +1,35 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Product } from "@/utils/products";
+import { Card, CardContent } from "@/components/ui/card";
+import { ProductList } from "@/types/Products.type";
 import { Star } from "lucide-react";
 
 interface ProductCardProps {
-  item: Product;
+  item: ProductList;
 }
 
 const ProductCard = ({ item }: ProductCardProps) => {
   return (
     <Card className="h-full border-2 rounded-none border-gray-100 transition-all  hover:border-black">
-    <CardContent className="flex flex-col justify-center p-0">
-      <div className="flex items-center justify-center h-80 w-full bg-gray-100">
-        <img src={item.img} className="h-full object-contain" alt={item.name} />
-      </div>
-      <div className="p-5">
-      <div className="flex justify-between items-center">
-        <p className="text-sm text-gray-500">
-          <span>{item.subCategory}</span>
-          <Star className="inline-block w-5 h-5 mb-1 text-[#edcf5d] fill-[#edcf5d]" /> 4.9
-        </p>
-      </div>
-      <p className="text-lg font-semibold">{item.name}</p>
-      <p className="font-semibold text-md">${item.price}</p>
-      </div>
-    </CardContent>
-  </Card>
+      <CardContent className="flex flex-col justify-center p-0">
+        <div className="flex items-center justify-center h-80 w-full bg-gray-100">
+          <img
+            src={item.img}
+            className="h-full object-contain"
+            alt={item.name}
+          />
+        </div>
+        <div className="p-5">
+          <div className="flex justify-between items-center">
+            <p className="text-sm text-gray-500">
+              <span>{item.subCategory.name}</span>
+              <Star className="inline-block w-5 h-5 mb-1 text-[#edcf5d] fill-[#edcf5d]" />{" "}
+              4.9
+            </p>
+          </div>
+          <p className="text-lg font-semibold">{item.name}</p>
+          <p className="font-semibold text-md">Rs {item.price.toNumber()}</p>
+        </div>
+      </CardContent>
+    </Card>
   );
 };
 
