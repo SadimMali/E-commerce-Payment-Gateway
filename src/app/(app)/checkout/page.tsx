72a560c9b983ec0ef230ec892a6691ca2cfd8510
Sadim Mali <sadimmalakar77@gmail.com>
2024-11-13 19:50:16 +0545

    feat: Interchange text dollar to rs

--- src/app/(app)/checkout/page.tsx
+++ src/app/(app)/checkout/page.tsx
@@ -223,19 +223,19 @@ const Page = () => {
             <div className="flex w-full items-center justify-between mt-2">
               <span className="font-semibold">Subtotal</span>
               {price.subTotalPrice ? (
-                <span>&#36;{price.subTotalPrice}</span>
+                <span>Rs {price.subTotalPrice}</span>
               ) : (
                 "-"
               )}
             </div>
             <div className="flex w-full items-center justify-between mt-2">
               <span className="font-semibold">Estimated Shipping</span>
-              {price.charge ? <span>&#36;{price.charge}</span> : "0"}
+              {price.charge ? <span>Rs {price.charge}</span> : "0"}
             </div>
             <div className="w-full border px-2 mt-3" />
             <div className="flex w-full items-center justify-between mt-2">
               <span className="font-semibold">Total</span>
-              {price.totalPrice ? <span>&#36;{price.totalPrice}</span> : "-"}
+              {price.totalPrice ? <span>Rs {price.totalPrice}</span> : "-"}
             </div>
             <div className="w-full border mt-2" />
           </div>
