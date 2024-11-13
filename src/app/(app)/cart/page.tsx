20012844caabfcf3308f3261da8189f62b82a295
Sadim Mali <sadimmalakar77@gmail.com>
2024-11-13 19:55:09 +0545

    style(Cart): text side

--- src/app/(app)/cart/page.tsx
+++ src/app/(app)/cart/page.tsx
@@ -9,10 +9,10 @@ const Page = () => {
         <section className="flex flex-col lg:flex-row md:px-5 xl:px-32 px-5 mt-16 gap-5">
           <article className="flex flex-col w-full lg:w-2/3 space-y-6">
             <section className="flex flex-col border p-2 order-2">
-              <h3 className="text-lg md:text-xl font-bold text-red-500">
+              <h3 className="text-md md:text-xl font-bold text-red-500">
                 Members get free shipping on orders $50+
               </h3>
-              <p className="text-gray-500 text-md">
+              <p className="text-gray-500 text-sm md:text-md">
                 Become a Nike Member for fast free shipping on orders $50+
               </p>
             </section>
