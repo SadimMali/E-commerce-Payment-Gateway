ea1b291bf3b030f93fd2aa54be2c3fb9c6a1e53b
Sadim Mali <sadimmalakar77@gmail.com>
2024-11-13 19:48:58 +0545

    refactor: Modify img tag with next/Image

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { ProductList } from "@/types/Products.type";
 import { Star } from "lucide-react";
+import Image from "next/image";
 
 interface ProductCardProps {
   item: ProductList;
@@ -11,11 +12,16 @@ const ProductCard = ({ item }: ProductCardProps) => {
     <Card className="h-full border-2 rounded-none border-gray-100 transition-all  hover:border-black">
       <CardContent className="flex flex-col justify-center p-0">
         <div className="flex items-center justify-center h-80 w-full bg-gray-100">
-          <img
-            src={item.img}
-            className="h-full object-contain"
-            alt={item.name}
-          />
+          <div className="relative w-full h-full">
+            {/* <img
+            /> */}
+            <Image
+              src={item.img}
+              className="absolute object-contain "
+              alt={item.name}
+              fill
+            />
+          </div>
         </div>
         <div className="p-5">
           <div className="flex justify-between items-center">
@@ -26,7 +32,7 @@ const ProductCard = ({ item }: ProductCardProps) => {
             </p>
           </div>
           <p className="text-lg font-semibold">{item.name}</p>
-          <p className="font-semibold text-md">Rs {item.price.toNumber()}</p>
+          <p className="font-semibold text-md">Rs {item.price}</p>
         </div>
       </CardContent>
     </Card>
