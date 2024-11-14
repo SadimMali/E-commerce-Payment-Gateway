5a3099de940ffbd4405eafd49889e72980d5eef0
Sadim Mali <sadimmalakar77@gmail.com>
2024-11-14 18:28:24 +0545

    feat(CartContext): Implement functionality to get and set cart item in localStorage

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,23 +1,49 @@
-"use client"
+"use client";
 import { ProductList } from "@/types/Products.type";
-import React, { createContext, Dispatch, SetStateAction, useState } from "react";
-
+import React, {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
 
 export interface Cart extends ProductList {
-    quantity: number;
+  quantity: number;
 }
 interface CartContextType {
-    cart: Cart[],
-    setCart: Dispatch<SetStateAction<Cart[]>>
+  cart: Cart[];
+  setCart: Dispatch<SetStateAction<Cart[]>>;
 }
 
 export const CartContext = createContext<CartContextType | null>(null);
 
-export const CartContextProvider = ({children}: {children: React.ReactNode})=>{
-    const [cart, setCart] = useState<Cart[]>([]);
-    return(
-        <CartContext.Provider value={{cart, setCart}}>
-            {children}
-        </CartContext.Provider>
-    )
-}
+const getCartFromLocalStorage = (): Cart[] => {
+  // execute only if window is typeof object
+  if (typeof window !== "undefined") {
+    const storedCart = localStorage.getItem("cart");
+    console.log(typeof storedCart)
+    return storedCart ? JSON.parse(storedCart) : [];
+  }
+  return [];
+};
+
+export const CartContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [cart, setCart] = useState<Cart[]>(getCartFromLocalStorage);
+
+  //Store cart in localStorage whenever there are changes in cart item
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  }, [cart]);
+  return (
+    <CartContext.Provider value={{ cart, setCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
