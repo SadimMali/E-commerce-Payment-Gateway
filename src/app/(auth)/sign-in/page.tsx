d7b85e50d71aafb5374690aa8baa679d8c313664
Sadim Mali <sadimmalakar77@gmail.com>
2024-11-16 20:45:47 +0545

    refactor:Replace static error message with dynamic error message from server

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -40,12 +40,11 @@ const LoginPage = () => {
       identifier: data.identifier,
       password: data.password,
     });
-    console.log(result);
     if (result?.error) {
       setIsSubmitting(false);
       toast({
         title: "Login failed",
-        description: "Incorrect username or password",
+        description: result?.error,
         variant: "destructive",
       });
     }
@@ -61,63 +60,62 @@ const LoginPage = () => {
   };
 
   return (
-    <div >
-          <h1 className="text-2xl font-semibold py-4">
-            Enter your email to join us or sign in 
-          </h1>
-        {/* sign in form */}
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-            <FormField
-              control={form.control}
-              name="identifier"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email / Username</FormLabel>
-                  <FormControl>
-                    <Input placeholder="email/username" {...field} />
-                  </FormControl>
+    <div>
+      <h1 className="text-2xl font-semibold py-4">
+        Enter your email to join us or sign in
+      </h1>
+      {/* sign in form */}
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+            control={form.control}
+            name="identifier"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email / Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="email/username" {...field} />
+                </FormControl>
 
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input type="password" placeholder="password" {...field} />
-                  </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="password" {...field} />
+                </FormControl>
 
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit" disabled={isSubmitting}>
-              {isSubmitting ? (
-                <>
-                  <Loader2 className="w-4 h-4 mr-2 animate-spin" /> Please
-                  wait...
-                </>
-              ) : (
-                "Sign in"
-              )}{" "}
-            </Button>
-          </form>
-        </Form>
-        <p className="text-md text-center mt-4 ">
-          Not a member yet?
-          <Link
-            href="/sign-up"
-            className=" ml-2 font-bold text-blue-600 hover:text-blue-800"
-          >
-            Sign up
-          </Link>
-        </p>
-      </div>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" disabled={isSubmitting}>
+            {isSubmitting ? (
+              <>
+                <Loader2 className="w-4 h-4 mr-2 animate-spin" /> Please wait...
+              </>
+            ) : (
+              "Sign in"
+            )}{" "}
+          </Button>
+        </form>
+      </Form>
+      <p className="text-md text-center mt-4 ">
+        Not a member yet?
+        <Link
+          href="/sign-up"
+          className=" ml-2 font-bold text-blue-600 hover:text-blue-800"
+        >
+          Sign up
+        </Link>
+      </p>
+    </div>
   );
 };
 
