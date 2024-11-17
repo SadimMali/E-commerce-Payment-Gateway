fc450d486551a2a91e27b5e2b2cc304cef3dd22a
Sadim Mali <sadimmalakar77@gmail.com>
2024-11-17 23:40:34 +0545

    feat(api): create api to confirm order after the payment success

---
+++ src/app/api/epayment/confirm/route.ts
@@ -0,0 +1,39 @@
+import { prisma } from "@/lib/prisma";
+
+export async function POST(req: Request) {
+  const { pidx, transaction_id, purchase_order_id } = await req.json();
+
+  const decodedOrderId = decodeURIComponent(purchase_order_id);
+  const decodedPaymentId = decodeURIComponent(pidx);
+  try {
+    await prisma.payment.update({
+      where: {
+        id: decodedPaymentId,
+      },
+      data: {
+        transactionId: transaction_id,
+      },
+    });
+    await prisma.order.update({
+      where: {
+        id: decodedOrderId,
+      },
+      data: {
+        status: "confirm",
+        paymentId: pidx,
+      },
+    });
+    return Response.json(
+      { success: true, message: "Order confirm" },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return Response.json(
+      {
+        success: false,
+        message: error.mesage || "Failed to confirm the order",
+      },
+      { status: 500 }
+    );
+  }
+}
