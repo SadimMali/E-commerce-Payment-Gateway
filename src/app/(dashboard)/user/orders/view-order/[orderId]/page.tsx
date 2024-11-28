ca82d80428f91a314d0075ab21af5a97f4ea157c
Sadim Mali <sadimmalakar77@gmail.com>
2024-11-28 22:17:33 +0545

    feat: Fetch single order by orderId  and pass as a props to component

---
+++ src/app/(dashboard)/user/orders/view-order/[orderId]/page.tsx
@@ -0,0 +1,36 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/options";
+import ViewOrder from "@/components/user/view-order";
+import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+
+const Page = async ({
+  params,
+}: {
+  params: { orderId: string | undefined };
+}) => {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!session && !user) {
+    return <div> Unauthorized</div>;
+  }
+  const { orderId } = params;
+
+  const order = await prisma.order.findUnique({
+    where: {
+      id: orderId,
+    },
+    include: {
+      orderItems: true,
+      delivery: true,
+      payment: true,
+    },
+  });
+  return (
+    <div className="">
+      <ViewOrder order={order} />
+    </div>
+  );
+};
+
+export default Page;
