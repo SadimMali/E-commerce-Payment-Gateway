ced577a229310894bf62882fd4f8315ccf0af87d
Sadim Mali <sadimmalakar77@gmail.com>
2024-11-28 22:15:25 +0545

    feat: Fetch user's orders and pass as a props to component

--- src/app/(dashboard)/user/orders/page.tsx
+++ src/app/(dashboard)/user/orders/page.tsx
@@ -1,11 +1,20 @@
-import UserOrder from "@/components/user/user-order"
+import { authOptions } from "@/app/api/auth/[...nextauth]/options";
+import UserOrder from "@/components/user/user-order";
+import { fetchOrderByUserId } from "@/services/orderService";
+import { getServerSession } from "next-auth";
 
-const Page = () => {
+const Page = async () => {
+  const session = await getServerSession(authOptions);
+
+  const user = session?.user;
+
+  const orders = await fetchOrderByUserId(user.id.toString());
+
   return (
-    <div className=''>
-      <UserOrder />
+    <div className="">
+      <UserOrder orders={orders} />
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
