295fbc02045dd13df6da8e8c1c27d9ddd1637d54
Sadim Mali <sadimmalakar77@gmail.com>
2024-12-21 21:32:45 +0545

    feat(Navbar): Add Dropdown NavigationMenu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,45 +1,62 @@
 "use client";
 import React, { useContext, useEffect, useState } from "react";
 import MaxWidthWraper from "./MaxWidthWrapper";
-import { Heart, ShoppingCart } from "lucide-react";
+import { Heart, ShoppingCart, Menu, X } from "lucide-react";
 import Link from "next/link";
 import { CartContext } from "@/context/CartContext";
 import { useSession } from "next-auth/react";
 import { User } from "next-auth";
 import { Cart } from "@/types/Cart.type";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "./ui/navigation-menu";
+import { cn } from "@/lib/utils";
+import { NavigationMenuList } from "@radix-ui/react-navigation-menu";
 
 const Navbar = () => {
-  const { data: session, status } = useSession(); // Get both session data and status
+  const { data: session, status } = useSession();
   const [userCart, setUserCart] = useState<Array<Cart>>([]);
+  const [isOpen, setIsOpen] = useState(false);
   const user: User = session?.user;
 
   const cartContext = useContext(CartContext);
 
-  //filter cart of an user if product exists in cart and user is authenticated
   useEffect(() => {
     if (cartContext && user?.id) {
       const filteredCart = cartContext.cart.filter((c) => c.userId === user.id);
       setUserCart(filteredCart);
-    } 
+    }
   }, [cartContext, user]);
-
 
   const quantity = userCart.reduce((acc, element) => {
     return acc + element.quantity;
   }, 0);
 
   const wish = []; // Replace this with actual wishlist logic
-  // Handle the loading state for the entire navbar
   if (status === "loading") {
     return (
-      <nav className="sticky z-10 inset-x-0 w-full top-0 h-14 border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+      <nav className="sticky z-20 inset-x-0 w-full top-0 h-14 border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
         <MaxWidthWraper>
           <div className="h-14 flex items-center justify-between">
             <Link href="/" className="font-semibold text-base">
               <span>E.com</span>
             </Link>
             <div className="flex items-center justify-between gap-5">
-              {/* Show loading indicator or skeletons */}
               <div className="flex space-x-5 items-center justify-between h-full">
                 <button className="bg-gray-200 w-24 h-8 animate-pulse rounded-md" />
                 <button className="bg-gray-200 w-24 h-8 animate-pulse rounded-md" />
@@ -51,13 +68,130 @@ const Navbar = () => {
     );
   }
 
+  const navItem = [
+    { name: "Home", path: "/" },
+    { name: "Products", path: "/products" },
+    { name: "Contact", path: "/contact" },
+  ];
+
+  const categoryComponent: { title: string; path: string }[] = [
+    {
+      title: "AllClothing",
+      path: "/products?category=allclothing",
+    },
+    {
+      title: "sneaker",
+      path: "/products?category=sneaker",
+    },
+    {
+      title: "Accessories",
+      path: "/products?categoryAccessories",
+    },
+    {
+      title: "tshirt",
+      path: "/products?category=tshirt",
+    },
+    {
+      title: "short",
+      path: "/products?category=short",
+    },
+    {
+      title: "backpack",
+      path: "/products?category=backpack",
+    },
+    {
+      title: "jacket",
+      path: "/products?category=jacket",
+    },
+  ];
+
+  const MobileNav = () => {
+    return (
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
+        <SheetTrigger asChild>
+          <Button variant="ghost" size="icon" className="md:hidden">
+            <Menu className="h-6 w-6" />
+            <span className="sr-only">Toggle menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className="w-[300px] sm:w-[400px]">
+          <SheetHeader>
+            <SheetTitle>
+              <Link
+                href="/"
+                onClick={() => setIsOpen(false)}
+                className="font-semibold text-xl"
+              >
+                E.com
+              </Link>
+            </SheetTitle>
+            <SheetDescription>Navigation menu</SheetDescription>
+          </SheetHeader>
+          <nav className="flex flex-col gap-4 mt-4">
+            <NavMenu />
+          </nav>
+        </SheetContent>
+      </Sheet>
+    );
+  };
+
+  const NavMenu = () => {
+    return (
+      <NavigationMenu>
+        <NavigationMenuList className="flex flex-col md:flex-row md:items-center gap-2">
+          {navItem.map((item, index) => (
+            <NavigationMenuItem key={index}>
+              <Link href={item.path} legacyBehavior passHref>
+                <NavigationMenuLink
+                  className={navigationMenuTriggerStyle()}
+                  onClick={() => setIsOpen(false)}
+                >
+                  {item.name}
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          ))}
+
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Category</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                {categoryComponent.map((component) => (
+                  <ListItem
+                    key={component.title}
+                    title={component.title}
+                    href={component.path}
+                    onClick={() => setIsOpen(false)}
+                  >
+                    {component.title}
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    );
+  };
+
   return (
-    <nav className="sticky z-10 inset-x-0 w-full top-0 h-14 border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+    <nav className="sticky z-20 inset-x-0 w-full top-0 h-14 border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWraper>
         <div className="h-14 flex items-center justify-between">
-          <Link href="/" className="font-semibold text-base">
-            <span>E.com</span>
-          </Link>
+          <div className="flex items-center justify-between gap-5 md:gap-16 text-sm md:txt-lg">
+            <div className="flex items-center gap-4">
+              <MobileNav />
+              <Link href="/" className="font-semibold text-base">
+                <span>E.com</span>
+              </Link>
+            </div>
+
+            <div className="hidden md:block">
+              <ul className="flex items-center justify-between gap-5">
+                <NavMenu />
+              </ul>
+            </div>
+          </div>
           <div className="flex items-center justify-between gap-5">
             <Link href="/cart">
               <button className="relative">
@@ -72,31 +206,57 @@ const Navbar = () => {
 
             <button className="relative">
               <Heart className="h-6 w-6" />
-              {wish.length > 0 && ( // TODO: : Change to wishlist logic
+              {wish.length > 0 && (
                 <span className="absolute flex items-center justify-center text-xs font-semibold w-4 h-4 p-2 border-2 rounded-full border-red-600 -right-4 -top-3">
                   {wish.length}
                 </span>
               )}
             </button>
 
-            {/* Show user info or Sign in / Sign up buttons */}
-            {user ? (
-              <Link href="/user">{user.username || "user"} ✨</Link>
-            ) : (
-              <div className="flex space-x-5 items-center justify-between h-full">
-                <Link href="/sign-up">
-                  <button>Sign up</button>
-                </Link>
-                <Link href="/sign-in">
-                  <button>Sign in</button>
-                </Link>
-              </div>
-            )}
+            {/* <div className="hidden md:block"> */}
+              {user ? (
+                <Link href="/user">{user.username || "user"} ✨</Link>
+              ) : (
+                <div className="flex space-x-5 items-center text-sm lg:text-[1rem] xl:text-base justify-between h-full">
+                  <Link href="/sign-up">
+                    <button>Sign up</button>
+                  </Link>
+                  <Link href="/sign-in">
+                    <button>Sign in</button>
+                  </Link>
+                </div>
+              )}
+            </div>
           </div>
-        </div>
+        {/* </div> */}
       </MaxWidthWraper>
     </nav>
   );
 };
 
 export default Navbar;
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none capitalize">
+            {title}
+          </div>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
