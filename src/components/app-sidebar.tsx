01374428b975d68f827567e7f49a7515fbcb0b9f
Sadim Mali <sadimmalakar77@gmail.com>
2024-12-21 21:33:27 +0545

    refactor: Modify url sidebar menu

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -14,64 +14,57 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 import Link from "next/link";
 
-// Menu items.
-const items = [
-  {
-    title: "Home",
-    url: "/",
-    icon: Home,
-    visible: ["ADMIN", "USER"],
-  },
-
-  // Admin-specific items
-  {
-    title: "Products",
-    url: "/admin/products",
-    icon: Computer,
-    visible: ["ADMIN"],
-  },
-  {
-    title: "Orders",
-    url: "/admin/orders",
-    icon: Logs,
-    visible: ["ADMIN"],
-  },
-  {
-    title: "Users",
-    url: "/admin/users",
-    icon: Users,
-    visible: ["ADMIN"],
-  },
-  // User-specific items
-  {
-    title: "My Orders",
-    url: "/user/orders",
-    icon: Logs,
-    visible: ["USER"],
-  },
-  {
-    title: "Profile",
-    url: "/user/profile",
-    icon: Users,
-    visible: ["USER"],
-  },
-  {
-    title: "Wishlist",
-    url: "/wishlist",
-    icon: Computer,
-    visible: ["USER"],
-  },
-  {
-    title: "Settings",
-    url: "/settings",
-    icon: Settings,
-    visible: ["ADMIN", "USER"],
-  },
-];
-
 export async function AppSidebar() {
   const session = await getServerSession(authOptions);
   const { role } = session?.user;
+
+  const items = [
+    {
+      title: "Home",
+      url: "/",
+      icon: Home,
+      visible: ["ADMIN", "USER"],
+    },
+
+    // Admin-specific items
+    {
+      title: "Products",
+      url: "/admin/products",
+      icon: Computer,
+      visible: ["ADMIN"],
+    },
+    {
+      title: "Orders",
+      url: "/admin/orders",
+      icon: Logs,
+      visible: ["ADMIN"],
+    },
+    {
+      title: "Users",
+      url: "/admin/users",
+      icon: Users,
+      visible: ["ADMIN"],
+    },
+    // User-specific items
+    {
+      title: "My Orders",
+      url: "/user/orders",
+      icon: Logs,
+      visible: ["USER"],
+    },
+    {
+      title: "Profile",
+      url: "/user/profile",
+      icon: Users,
+      visible: ["USER"],
+    },
+    {
+      title: "Settings",
+      url: `/${role.toLowerCase()}/setting`,
+      icon: Settings,
+      visible: ["ADMIN", "USER"],
+    },
+  ];
   return (
     <Sidebar>
       <SidebarContent>
