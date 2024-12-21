0ed35c024fc3faa6c6530595d2a0b167dc81f6a0
Sadim Mali <sadimmalakar77@gmail.com>
2024-12-21 23:35:25 +0545

    feat: Include subcategory field

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -5,12 +5,14 @@ import SpecialOffer from "@/components/hompage/SpecialOffer";
 import FeaturedProduct from "@/components/hompage/FeaturedProduct";
 import { prisma } from "@/lib/prisma";
 import Newsletter from "@/components/hompage/Newsletter";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 export default async function HomePage() {
   const featuredProducts = await prisma.product.findMany({
     take: 5,
     include: {
       category: true,
+      subCategory: true,
     },
     orderBy: {
       releaseDate: "desc",
@@ -20,15 +22,18 @@ export default async function HomePage() {
     <div className="flex flex-col min-h-screen">
       {/* Hero Section */}
       <Hero />
+      {/* Categories */}
+      <MaxWidthWrapper>
+        <Category />
+      </MaxWidthWrapper>
       {/* Features */}
       <Features />
-      {/* Categories */}
-      <Category />
       {/* Featured Products */}
       <FeaturedProduct featuredProducts={featuredProducts} />
-
       {/* Special Offer */}
-      <SpecialOffer />
+      <MaxWidthWrapper>
+        <SpecialOffer />
+      </MaxWidthWrapper>
       {/* Newsletter */}
       <Newsletter />
     </div>
